dcea41217161170dbbc6dcb4c6cd20789c5fbe2c
Eduardo Theiji Yshimaro <eduardo.yshimaro@prestadores.amil.com.br>
2017-10-17 08:56:33 -0200

    projetos novos

---
+++ CursosAlura/Paralelismo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+
+namespace Paralelismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FatorialEmParalelo(5);
+        }
+
+        public static long FatorialEmParalelo(int valor)
+        {
+            long resultado = 0;
+
+            var t_calculo = new Thread(() => resultado = Fatorial(valor));
+            t_calculo.Start();
+
+            return resultado;
+        }
+
+        private static long Fatorial(int valor)
+        {
+            var resultado = 1L;
+            for (int i = valor; i >= 2; i--)
+                resultado *= i;
+            return resultado;
+        }
+    }
+}
